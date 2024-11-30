a89bc82db6a1fa1a8f572da5064748a08c460164
Jack <jackfarrell860@gmail.com>
2024-11-29 22:53:34 -0500

    updated ts func

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { ReactFlow } from '@xyflow/react';
-import { Slide } from './components/Slide';
-import { Doc } from './components/Doc';
 
-const nodeTypes = {
-  doc: Doc,
-};
 
-export default function App() {
-  const nodes = [
-    { id: '0', type: 'doc', position: { x: 0, y: 0 }, data: {} },
-  ];
-
-  return <ReactFlow nodes={nodes} nodeTypes={nodeTypes} fitView />;
+function App() {
+  return (
+    <div>
+      Hello, World
+    </div>
+  );
 }
+
+export default App;
